4d1376707299d5fc704f0242eb520a0c6d973d69
Honghao Zhang <zhh358@gmail.com>
2014-09-25 20:14:00 -0400

    Initial Commit

---
+++ DynamicCollectionViewCellWithAutoLayout-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicCollectionViewCellWithAutoLayout-Demo
+//
+//  Created by Honghao Zhang on 2014-09-25.
+//  Copyright (c) 2014 HonghaoZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
